6669e2f6301a8a1be7192aeca0eca8d3fd553938
Tóth Dániel <tdaniel9854@gmail.com>
2019-05-10 21:09:11 +0200

    seems like -a is not what i thougt it is :(

---
+++ server/dotnetforum.DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetforum.DAL.Entities
+{
+    public class ApplicationUser  : IdentityUser<int>
+    {
+    
+        public ICollection<Review> Reviews { get; } = new List<Review>();
+
+        public ICollection<Comment> Comments { get; } = new List<Comment>();
+
+
+    }
+}
